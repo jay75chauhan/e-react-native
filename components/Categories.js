4ecc7d09d29a6e33b7097c8eeefaeff7160f60a6
jay75chauhan <chauhan75jayk@gmail.com>
2022-02-28 20:28:20 +0530

    Add item

--- components/Categories.js
+++ components/Categories.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
@@ -13,18 +13,22 @@ export default function Categories({ setCategories }) {
     return () => data;
   }, []);
   return (
-    <View style={tw`flex-row w-full p-2  justify-between`}>
-      {title?.map((item, index) => (
-        <TouchableOpacity
-          onPress={() => {
-            setCategories(item);
-          }}
-          key={index}
-          style={tw`mx-1 bg-white rounded-2xl shadow-xl`}
-        >
-          <Text style={tw`px-2.5 py-2 text-xs text-black`}>{item}</Text>
-        </TouchableOpacity>
-      ))}
+    <View
+      style={tw`flex-row  p-1 py-2 bg-transparent bg-white rounded-b-2xl shadow-xl  justify-between`}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {title?.map((item, index) => (
+          <TouchableOpacity
+            onPress={() => {
+              setCategories(item);
+            }}
+            key={index}
+            style={tw`mx-1 bg-gray-200 rounded-2xl `}
+          >
+            <Text style={tw`px-2.5 py-2 text-xs text-black`}>{item}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
